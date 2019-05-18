1f50f7ba021ed0233907c054eaad4ed309e5a7fa
dbertranou-eda <daniela.bertranou@alumnos.frm.utn.edu.ar>
2019-05-18 17:08:47 -0300

    refactored implementation

--- exercise1.py
+++ exercise1.py
@@ -9,57 +9,79 @@ DIGITS = '0123456789'
 SIZE = 4
 
 
-def generate_number():
-    reference_number = ''.join(sample(DIGITS, SIZE))
-    return reference_number
+class Game:
+    def __init__(self, game_type):
+        self.game_type = game_type
 
+    def generate_secret(self):
+        secret_number = ''.join(sample(DIGITS, SIZE))
+        return secret_number
 
-class NumberGame:
-    def __init__(self, reference_number):
-        self.reference_number = reference_number
+    def validate_input(self, some_input):
+        try:
+            int(some_input)
+        except ValueError:
+                return False
+        return True
+
+    def validate_game(self):
+        if not self.validate_input(self.game_type):
+            return False
+        self.game_type = int(self.game_type)
+        if self.game_type != 1 and self.game_type != 2:
+            return False
+        return True
+
+    def check_numbers(self, guess_number, secret_number):
+        result = {'good': 0, 'regular': 0}
+        for g, n in zip(guess_number, secret_number):
+            if g == n:
+                result['good'] += 1
+            elif g in secret_number:
+                result['regular'] += 1
+        return result
 
     def play(self):
+        if self.game_type == 1:
+            secret_number = self.generate_secret()
+            guesser_game = GuesserGame(secret_number)
+            guesser_game.play_guesser()
+        elif game.game_type == 2:
+            # game.play_thinker()
+            pass
+
+
+class GuesserGame(Game):
+    def __init__(self, secret_number):
+        self.secret_number = secret_number
+
+    def validate_guess(self, guess_number):
+        if not self.validate_input(guess_number):
+            return False
+        if len(guess_number) != SIZE:
+            return False
+        return True
+
+    def play_guesser(self):
         guess_number = input('Enter a {} digit number --> '.format(SIZE))
-        guess = Guess(guess_number)
-        while not guess.validate_guess():
+        while not self.validate_guess(guess_number):
             print(CRED, 'You did not put a {} digit number. Try again.'.format(
                 SIZE), CEND)
-            guess.guess_number = input('Enter a {} digit number --> '.format(
-                SIZE))
-        result = guess.check_guess(self.reference_number)
+            guess_number = input('Enter a {} digit number --> '.format(SIZE))
+        result = self.check_numbers(guess_number, self.secret_number)
         if result['good'] == SIZE:
             print(CGREEN, 'Great! You win!', CEND)
             sys.exit()
         print('You got {} good and {} regular. Try again.'.format(
             result['good'], result['regular']))
-        self.play()
-
-
-class Guess:
-    def __init__(self, guess_number):
-        self.guess_number = guess_number
-
-    def validate_guess(self):
-        try:
-            int(self.guess_number)
-        except ValueError:
-                return False
-        if len(self.guess_number) != SIZE:
-            return False
-        return True
-
-    def check_guess(self, reference_number):
-        result = {'good': 0, 'regular': 0}
-        for g, n in zip(self.guess_number, reference_number):
-            if g == n:
-                result['good'] += 1
-            elif g in reference_number:
-                result['regular'] += 1
-        return result
+        self.play_guesser()
 
 
 if __name__ == '__main__':
-    reference_number = generate_number()
-    number_game = NumberGame(reference_number)
-    # print(reference_number)
-    number_game.play()
+    print('Welcome to the Guessing Game!\nWho you want to be? A Guesser(1) or a Thinker(2).')
+    game_type = input('Enter 1 or 2 to select the mode --> ')
+    game = Game(game_type)
+    while not game.validate_game():
+        print('We do not have that kind of game, sorry.')
+        game.game_type = input('Enter 1 or 2 to select the mode --> ')
+    game.play()
--- tests/tests.py
+++ tests/tests.py
@@ -1,29 +1,31 @@
 import unittest
-from exercise1 import Guess, SIZE
+from exercise1 import GuesserGame, SIZE
 
 
 class GuessTest(unittest.TestCase):
     def test_validate_guess_with_string_value_returns_false(self):
+        SECRET_NUMBER = '1234'
         TEST_VALUE = 'cool'
-        guess = Guess(TEST_VALUE)
-        self.assertFalse(guess.validate_guess())
+        guesser_game = GuesserGame(SECRET_NUMBER)
+        self.assertFalse(guesser_game.validate_guess(TEST_VALUE))
 
     def test_validate_guess_with_invalid_number_returns_false(self):
+        SECRET_NUMBER = '1234'
         TEST_VALUE1 = '123'
         TEST_VALUE2 = '12345'
-        guess1 = Guess(TEST_VALUE1)
-        guess2 = Guess(TEST_VALUE2)
-        self.assertFalse(guess1.validate_guess())
-        self.assertFalse(guess2.validate_guess())
+        guesser_game = GuesserGame(SECRET_NUMBER)
+        self.assertFalse(guesser_game.validate_guess(TEST_VALUE1))
+        self.assertFalse(guesser_game.validate_guess(TEST_VALUE2))
 
-    def test_check_guess_with_same_numbers_returns_size_value_as_good(self):
-        TEST_NUMBER = '1234'
-        guess = Guess(TEST_NUMBER)
-        result = guess.check_guess(TEST_NUMBER)
+    def test_check_numbers_with_same_numbers_returns_size_value_as_good(self):
+        SECRET_NUMBER = '1234'
+        guesser_game = GuesserGame(SECRET_NUMBER)
+        result = guesser_game.check_numbers(SECRET_NUMBER, SECRET_NUMBER)
         self.assertEqual(result['good'], SIZE)
 
-    def test_check_guess_with_different_numbers_does_not_return_size_value_as_good(self):
-        number = '1234'
-        guess = Guess('1235')
-        result = guess.check_guess(number)
+    def test_check_numbers_with_different_numbers_does_not_return_size_value_as_good(self):
+        SECRET_NUMBER = '1234'
+        TEST_VALUE = '1235'
+        guesser_game = GuesserGame(SECRET_NUMBER)
+        result = guesser_game.check_numbers(TEST_VALUE, SECRET_NUMBER)
         self.assertNotEqual(result['good'], SIZE)
