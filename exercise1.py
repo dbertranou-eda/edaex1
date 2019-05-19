0fcafcdfc6eeb3632895e78b3f11203ec7533ac0
dbertranou-eda <daniela.bertranou@alumnos.frm.utn.edu.ar>
2019-05-19 19:33:28 -0300

    refactored play implementations

--- exercise1.py
+++ exercise1.py
@@ -9,8 +9,8 @@ SIZE = 4
 
 
 class Game:
-    def __init__(self, game_type):
-        self.game_type = game_type
+    def __init__(self):
+        self.game_type = None
 
     def generate_secret(self):
         secret_number = ''.join(sample(DIGITS, SIZE))
@@ -26,11 +26,14 @@ class Game:
         while True:
             value = input(prompt)
             try:
-                int(value)
-                return value
+                val = int(value)
             except ValueError:
                 cprint('Sorry, that is not a valid input. Try again.', 'red')
                 continue
+            if val >= 0:
+                return value
+            else:
+                cprint('Sorry, that is not a valid input. Try again.', 'red')
 
     def validate_game(self):
         self.game_type = int(self.game_type)
@@ -46,6 +49,11 @@ class Game:
             elif g in secret_number:
                 result['regular'] += 1
         return result
+
+    def is_over(self, result):
+        if result['good'] == SIZE:
+            cprint('Excellent!', 'green')
+            sys.exit()
 
     def play(self):
         if self.game_type == 1:
@@ -63,23 +71,20 @@ class GuesserGame(Game):
         self.secret_number = secret_number
 
     def validate_guess(self, guess_number):
-        if len(guess_number) != SIZE:
+        if len(guess_number) != SIZE or len(set(guess_number)) != SIZE:
             return False
         return True
 
     def play_guesser(self):
-        guess_number = Game.valid_input('Enter a {} digit number --> '.format(SIZE))
-        while not self.validate_guess(guess_number):
-            cprint('You did not put a {} digit number. Try again.'.format(
-                SIZE), 'red')
-            guess_number = Game.valid_input('Enter a {} digit number --> '.format(SIZE))
-        result = self.check_numbers(guess_number, self.secret_number)
-        if result['good'] == SIZE:
-            cprint('Great! You won!', 'green')
-            sys.exit()
-        print('You got {} good and {} regular. Try again.'.format(
-            result['good'], result['regular']))
-        self.play_guesser()
+        while True:
+            guess_number = Game.valid_input('Enter a {}-digit number --> '.format(SIZE))
+            if not self.validate_guess(guess_number):
+                cprint('You did not put a number with {} unique digits. Try again.'.format(SIZE), 'red')
+                continue
+            result = self.check_numbers(guess_number, self.secret_number)
+            self.is_over(result)
+            print('You got {} good and {} regular. Try again.'.format(
+                result['good'], result['regular']))
 
 
 class ThinkerGame(Game):
@@ -91,43 +96,48 @@ class ThinkerGame(Game):
             return False
         return True
 
+    def set_choices(self, result):
+        new_choices = []
+        for c in self.choices:
+            if self.check_numbers(c, self.choices[0]) == result:
+                new_choices.append(c)
+        self.choices = new_choices
+
+    def check_choices(self, answers, results):
+        if not self.choices:
+            cprint('Game over! Nothing fits those scores you gave:', 'white')
+            for a, r in zip(answers, results):
+                cprint(' {} -> Good: {}, Regular: {}'.format(
+                    ''.join(a), r['good'], r['regular']), 'white')
+            sys.exit()
+
     def play_thinker(self):
         answers = []
         results = []
         result = {}
         while True:
             ans = self.choices[0]
-            answers.append(ans)
             print(''.join(ans))
             result['good'] = int(Game.valid_input('How many good?--> '))
             result['regular'] = int(Game.valid_input('How many regular?--> '))
-            while not self.validate_result(result):
+            if not self.validate_result(result):
                 cprint('Oops! Those scores seem incongruent. Try again.', 'red')
-                result['good'] = int(Game.valid_input('How many good?--> '))
-                result['regular'] = int(Game.valid_input('How many regular?--> '))
+                continue
+            answers.append(ans)
             results.append(dict(result))
-            if result['good'] == SIZE:
-                cprint('Great! I won!', 'green')
-                sys.exit()
-            new_choices = []
-            for c in self.choices:
-                if self.check_numbers(c, ans) == result:
-                    new_choices.append(c)
-            self.choices = new_choices
-            if not self.choices:
-                cprint('Game over! Nothing fits those scores you gave:', 'white')
-                for a, r in zip(answers, results):
-                    cprint(' {} -> Good: {}, Regular: {}'.format(
-                        ''.join(a), r['good'], r['regular']), 'white')
-                sys.exit()
+            self.is_over(result)
+            self.set_choices(ans, result)
+            self.check_choices(answers, results)
+
 
 
 if __name__ == '__main__':
     cprint('Welcome to the Guessing Game!\
-        \nWho you want to be? A Guesser(1) or a Thinker(2).', 'magenta')
-    game_type = Game.valid_input('Enter 1 or 2 to select the mode --> ')
-    game = Game(game_type)
-    while not game.validate_game():
-        cprint('We do not have that kind of game, sorry.', 'red')
+        \nWho you want to be?\n Guesser(1)\n Thinker(2)', 'magenta')
+    game = Game()
+    while True:
         game.game_type = Game.valid_input('Enter 1 or 2 to select the mode --> ')
+        if game.validate_game():
+            break
+        cprint('We do not have that kind of game, sorry.', 'red')
     game.play()
